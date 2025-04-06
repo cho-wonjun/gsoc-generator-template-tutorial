219aca7410986e704e747cba86df9d387c8d8020
Won Jun Cho <wonjun2004@gmail.com>
2025-04-06 04:09:13 -0700

    Add files via upload

---
+++ python-mqtt-client-template/test/project/client.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+
+mqttBroker = "test.mosquitto.org"
+
+class TemperatureServiceClient:
+
+  def __init__(self):
+              self.client = mqtt.Client()
+              self.client.connect(mqttBroker)
+
+  def sendTemperatureChange(self, id):
+            topic = "temperature/changed"
+            self.client.publish(topic, id)
+  def sendTemperatureDrop(self, id):
+            topic = "temperature/dropped"
+            self.client.publish(topic, id)
+  def sendTemperatureRise(self, id):
+            topic = "temperature/risen"
+            self.client.publish(topic, id)
+
+
